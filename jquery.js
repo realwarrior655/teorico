6d593bc9881f55f299e164ae67adefa7fa0602f2
bazan ivan <ivanbazan12xd@gmail.com>
2023-10-21 16:53:27 -0300

    commit de proyecto teorico

---
+++ jquery.js
@@ -0,0 +1,11 @@
+//jquery es una libreria que te permite escribir codigo js de forma simplificada y con funcionalidades nuevas.
+
+//para usarla debes colocar el CDN de jquery en el archivo html a trabajar, el script es este
+
+<script
+    src="https://code.jquery.com/jquery-3.7.1.js"
+    integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+    crossorigin="anonymous"></script>
+
+//los SCRIPTs y CDNs se colocan dentro de la etiqueta <head> de los archivos HTML
+//actualmente Jquery es totalmente opcional y se recomienda utilizar las herramientas de JS modernas
